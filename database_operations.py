0fcb43caf699f98a7881ec7c65dbb6d54e29ccfe
stasialorentsen <stasia.lorentsen@gmail.com>
2024-02-20 22:17:01 +0100

    First commit

---
+++ database_operations.py
@@ -0,0 +1,22 @@
+# Function to create a new person in the database
+def create_person(tx, person_details):
+    # Cypher query to create a new person node with provided details
+    create_person_query = "CREATE (p:Person {name: $name, surname: $surname, birthdate: $birthdate})"
+    # Running the query using the transaction passed as an argument, and passing person_details as parameters
+    tx.run(create_person_query, **person_details)
+
+# Function to search for persons in the database based on provided parameters
+def search_person(tx, search_param):
+    # Cypher query to search for persons by name or surname
+    search_person_query = (
+        "MATCH (p:Person) "
+        "WHERE p.name = $name OR p.surname = $surname "
+        "RETURN p.name AS name, p.surname AS surname, p.birthdate AS birthdate"
+    )
+    # Splitting the search parameter into name and surname (if applicable)
+    name, *surname = search_param.split(" ", 1)
+    surname = " ".join(surname) if surname else None
+    # Running the query using the transaction passed as an argument, and passing name and surname as parameters
+    result = tx.run(search_person_query, name=name, surname=surname)
+    # Returning a list of dictionaries containing the search results
+    return [{"name": record['name'], "surname": record['surname'], "birthdate": record['birthdate']} for record in result]
